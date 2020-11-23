3d5faf543025ed9d45d26ae6522c96d8a730684b
achernenkov <jardagorng@gmail.com>
2020-11-23 21:56:42 +0300

    Updates

---
+++ precache-manifest.bc8ba8d66472737e46a4582fb3e16baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f5a6d3cae5b1945b09ba242f55736f",
+    "url": "/IT-Incubator-IgnatDZ/index.html"
+  },
+  {
+    "revision": "c17d49e8c3f7d7431f2e",
+    "url": "/IT-Incubator-IgnatDZ/static/css/main.2a4ada76.chunk.css"
+  },
+  {
+    "revision": "8c19636d566d75a8f260",
+    "url": "/IT-Incubator-IgnatDZ/static/js/2.9ad9980e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IT-Incubator-IgnatDZ/static/js/2.9ad9980e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17d49e8c3f7d7431f2e",
+    "url": "/IT-Incubator-IgnatDZ/static/js/main.0ca1e3b0.chunk.js"
+  },
+  {
+    "revision": "e6a310cb33965ee27a9c",
+    "url": "/IT-Incubator-IgnatDZ/static/js/runtime-main.c99e9d0f.js"
+  }
+]);
