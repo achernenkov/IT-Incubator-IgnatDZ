7c293aca7d57a871fe07b30097540ed0886d662c
achernenkov <jardagorng@gmail.com>
2020-12-20 14:39:50 +0300

    Completed logic.

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
+type loadingACType = {
+    type: 'LOADING-CHANGE'
+    payload: {loading:boolean}
+}
+export type HW10State = {
+    loading: boolean
+}
 
+const initState: HW10State = {
+    loading: false
 };
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: loadingACType): HW10State => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "LOADING-CHANGE": {
+            return {...state,...action.payload}
         }
-        default: return state;
+        default:
+            return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (loading:boolean): loadingACType => {
+    return {
+        type: 'LOADING-CHANGE',
+        payload: {loading}
+    }
+};
